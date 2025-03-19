296f4e9e669e8bd45266e3b57c498e385923cc42
BronzyPlum6390 <boxton.dtcc@gmail.com>
2025-03-19 15:20:34 +0900

    Got rid of comments

--- 1.py
+++ 1.py
@@ -1,11 +1,3 @@
-'''
-Write a Python program that shows multiples of 2 and multiples of 3. Your program takes an
-integer n as an input from a user and shows the multiples of 2 and multiples of 3 from 1 to n.
-Your program should work as the following example. (⏎ represents a ‘Enter’ key stroke from a
-user).
-
-
-'''
 
 def print_multiples(n):
     for i in range(1, n+1):
--- 3.py
+++ 3.py
@@ -1,15 +1,3 @@
-"""
-
-Write a Python program that takes an integer n as an input from a user and shows the return
-value of the addTotal function and the gMul variable. Your program should work as the
-following example. (⏎ represents a ‘Enter’ key stroke from a user).
-A. addTotal function: takes an integer parameter and returns the sum of the all integers
-from one to the given parameter.
-B. mulTotal function: takes an integer parameter, computes the multiplication of the all
-integers from one to the given parameter, and stores the multiplication result to the global
-variable gMul. muTotal returns nothing.
-
-"""
 
 
 def addTotal(n):
